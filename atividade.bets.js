50057e899d95413ef4c8efa18b8bdf611724f35d
anaclaramicheletto <162596585+anaclaramicheletto@users.noreply.github.com>
2024-10-24 14:08:43 -0300

    first commit

---
+++ atividade.bets.js
@@ -0,0 +1,26 @@
+import {Router} from "express";
+
+const suspeitosRoutes= Router();
+
+let suspeitos = [
+       id: 1,
+       nome: "dr deolane",
+       profissao: "Advogada"
+       apostas: true,
+       nivel:"médio",
+]
+
+let suspeitos = [
+       id: 2,
+       nome: "MC IG",
+       profissao: "Cantor"
+       apostas: true,
+       nivel:"alto",
+]
+let suspeitos = [
+       id: 3,
+       nome: "MC GP",
+       profissao: "Cantor"
+       apostas: true,
+       nivel:"médio",
+]
